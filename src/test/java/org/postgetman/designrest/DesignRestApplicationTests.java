28d906d7869dd5edc2ec982d67cd5d8f20e70672
postgetman <postgetman@gmail.com>
2017-12-02 18:12:52 +0200

    add base files of project

---
+++ src/test/java/org/postgetman/designrest/DesignRestApplicationTests.java
@@ -0,0 +1,16 @@
+package org.postgetman.designrest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DesignRestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
