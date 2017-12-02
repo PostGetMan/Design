f6ecd9f0562f08b640a7a862e1b626c9dc4c604b
postgetman <postgetman@gmail.com>
2017-12-02 18:10:43 +0200

    create Abstract entity and first packages

---
+++ src/main/java/org/postgetman/designrest/DesignRestApplication.java
@@ -0,0 +1,12 @@
+package org.postgetman.designrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignRestApplication.class, args);
+	}
+}
---
+++ src/main/java/org/postgetman/designrest/domain/AbstractEntity.java
@@ -0,0 +1,11 @@
+package org.postgetman.designrest.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AbstractEntity {
+
+    private Long id;
+}
